98f309aeb9e94620005b684a26c1973c3615de4c
dsrkreddy81 <74000937+dsrkreddy81@users.noreply.github.com>
2025-01-14 11:18:24 -0500

    412

---
+++ Fizz Buzz.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> x = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if((i%3==0) && (i%5==0)){
+                x.add("FizzBuzz");
+            }
+            else if(i%3==0){
+                x.add("Fizz");
+            }
+            else if(i%5==0){
+                x.add("Buzz");
+            }
+            else{
+                x.add(String.valueOf(i));
+            }
+        }
+        return x;
+    }
+}
